27eabb26684ba6f7908a8e3a326b95f57eaa433e
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-06-12 14:21:42 +0330

    base page and dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const page = async () => {
+    return (
+        <div className="flex flex-col items-center gap-10 w-full p-4 grow">
+            <h1 className="texl">Welcome To Dashboard</h1>
+        </div>
+    );
+};
+
+export default page;
