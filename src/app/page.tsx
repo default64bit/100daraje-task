45dfb01316319b4130ec235b10635dd30867e42b
Kasra Keshvardoost <kasrakeshvardoost@gmail.com>
2025-06-12 15:06:38 +0330

    home page dynamic link for easy navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
+import { cookies } from "next/headers";
+
 export default async function Home() {
-    return <div className=""></div>;
+    const cookiesStored = await cookies();
+    const userData = cookiesStored.get("user");
+
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <h1 className="text-2xl font-bold">Welcome</h1>
+            {!!userData ? (
+                <Link href="/dashboard">
+                    <Button>Go To Dashboard</Button>
+                </Link>
+            ) : (
+                <Link href="/login">
+                    <Button>Login</Button>
+                </Link>
+            )}
+        </div>
+    );
 }
